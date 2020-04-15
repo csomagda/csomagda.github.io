bb0723f39747855a83aca1c72e4e8bdb5fdd8ce4
csomagda <miklosne.magdi@gmail.com>
2020-04-15 19:11:48 +0200

    First
    First steps

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - web_20200413</title>
+  <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Josefin+Sans&family=Merriweather&display=swap" rel="stylesheet"><link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<header>
+  <h3>Hi, my name is </h3>
+<h1>Anakin Skywalker</h1>
+  </header>
+<div>
+  <p><strong>Analytical, goal oriented leader</strong> with proven track record in management and enforcement Space Fleet Operations or Frontend Development. Wide range of hands-on experience, including:</p>
+
+<ul><li>Large scale construction projects oversight</li>
+<li>Advanced forms of mind control</li>
+   <li>Planetary scale demolitions operations</li>
+<li>Fugitive recovery services</li>
+
+  </ul>
+<blockquote>Together we can rule the galaxy</blockquote>
+<h2>Notable achievements</h2>
+<p class="smaller">Designed, built and piloted a custom pod racer. 32 BBY Boonta Eve Classic Race Champion</p>
+  </div>
+<!-- partial -->
+  
+</body>
+</html>
